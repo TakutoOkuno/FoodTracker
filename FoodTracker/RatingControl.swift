51bd19f7f0cbb0eabe3573728284ed0a07d92f8b
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2018-07-05 21:50:48 +0900

    Approach Implement a Custom Control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 奧野拓人 on 2018/07/05.
+//  Copyright © 2018年 奧野拓人. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    // MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
