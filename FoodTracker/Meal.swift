7a1aadab8ef10abc07ec671be2671ee66ebd79bf
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2018-07-24 23:04:09 +0900

    To define a data model for a meal

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018年 奧野拓人. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
